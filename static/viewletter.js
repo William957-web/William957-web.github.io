992a63dc91a54f8b78fec9293181634eb4a6d6b5
William957-web <85293841+William957-web@users.noreply.github.com>
2024-04-18 09:12:01 +0800

    Update viewletter.js

--- static/viewletter.js
+++ static/viewletter.js
@@ -1,13 +1,13 @@
-fetch("http://127.0.0.1:80/message/3").then((r) => {
+fetch("http://127.0.0.1/message/3").then((r) => {
+
     return r.text();
+
 }).then((x) => {
-    fetch("http://127.0.0.1:80/submit", {
+    fetch("http://127.0.0.1/submit", {
         "headers": {
             "content-type": "application/json"
         },
         "body": x,
         "method": "POST",
-        "mode": "cors",
-        "credentials": "omit"
     });
 });
