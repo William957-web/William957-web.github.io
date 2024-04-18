7a0144562b07c2581550af7b8ba8eea4a7070e64
William957-web <85293841+William957-web@users.noreply.github.com>
2024-04-18 09:23:21 +0800

    Update viewletter.js

--- viewletter.js
+++ viewletter.js
@@ -1,7 +1,5 @@
-fetch("http://127.0.0.1/message/3").then((r) => {
-
-    return r.text();
-
+fetch("http://127.0.0.1/message/3").then(res => {
+    return res.text();
 }).then((x) => {
     fetch("http://127.0.0.1/submit", {
         "headers": {
